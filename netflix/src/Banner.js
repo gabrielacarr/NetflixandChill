f651cf862641752c0cebd7f99305c34c3213f1d3
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-20 18:42:01 -0400

    ✨ Add: API async fnc to render

--- netflix/src/Banner.js
+++ netflix/src/Banner.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Request";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
   // fnc: description charac limit
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
@@ -12,7 +30,7 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://latinamedia.co/wp-content/uploads/2020/02/Netflix-You-Banner.jpg")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
