167e282dc3dea98f45fac2f3986f2ff6753a1c34
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-20 15:47:52 -0400

    ✨ Add: Nav Animation + Transition

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -5,7 +5,6 @@ import HomeScreen from "./HomeScreen";
 function App() {
   return (
     <div className="app">
-      <h1>Build foundation complete</h1>
       <HomeScreen />
     </div>
   );
--- netflix/src/HomeScreen.js
+++ netflix/src/HomeScreen.js
@@ -6,6 +6,7 @@ function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
+
       {/* Banner */}
       {/* Row */}
     </div>
