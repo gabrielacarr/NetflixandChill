924d4130f1aa6bcc3cf068f884083383f96bca09
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-20 14:02:44 -0400

    📝  React project with Redux + Firebase

---
+++ netflix/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZjrdAkS5OHLDVn4AEwwfBl0tTk6riMbk",
+  authDomain: "netflix-clone-d9543.firebaseapp.com",
+  projectId: "netflix-clone-d9543",
+  storageBucket: "netflix-clone-d9543.appspot.com",
+  messagingSenderId: "534479023359",
+  appId: "1:534479023359:web:6b83918517aeafbecdf0ca",
+};
