c9e6de2d0267eae6d381ab8522ad4f0d41b085b5
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-20 14:29:28 -0400

    📝 Add: HomeScreen + Nav component

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import HomeScreen from "./HomeScreen";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Build foundation complete</h1>
+      <HomeScreen />
     </div>
   );
 }
