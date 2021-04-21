1e511aa2556e6ab7dcb968d70c26f3533e65624b
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-21 12:53:08 -0400

    🗑 Fix: Deprecated dependencies (Firebase cli)

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 import "./Row.css";
 
-function Row({ title }) {
+function Row({ title, fetchURL, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
   return (
     <div className="row">
       <h2>{title}</h2>
+      <div className="rowPosters">
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.poster_path)) && (
+              <img
+                className={`rowPoster ${isLargeRow && `rowPosterLarge`}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
     </div>
   );
 }
