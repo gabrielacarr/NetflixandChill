4a88501be78a0be241ae36e8455a2ec79372daa9
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-26 18:01:37 -0400

    ✨ Add: Login feat

--- netflix/src/screens/LoginScreen.js
+++ netflix/src/screens/LoginScreen.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./LoginScreen.css";
 
 function LoginScreen() {
-  return <div className="loginScreen"></div>;
+  return (
+    <div className="loginScreen">
+      <div className="loginScreenBackground">
+        <img
+          src="https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default LoginScreen;
