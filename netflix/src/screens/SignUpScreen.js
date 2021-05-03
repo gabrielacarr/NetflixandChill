176f66ea81b6cab77ebe2302a85c518512dded7b
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-05-03 14:39:24 -0400

    ⚡️ Add: onClick for  signIn + signUp

--- netflix/src/screens/SignUpScreen.js
+++ netflix/src/screens/SignUpScreen.js
@@ -2,16 +2,29 @@ import React from "react";
 import "./SignUpScreen.css";
 
 function SignUpScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signUpScreen">
       <form>
         <h1>Sign In</h1>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+
         <h4>
           <span className="signUpScreenGray">New to Netflix? </span>
-          Sign Up Now.
+          <span className="signUpScreenLink" onClick={register}>
+            Sign Up Now.
+          </span>
         </h4>
       </form>
     </div>
